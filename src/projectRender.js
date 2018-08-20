7cc19a3f2da8c75f10d44a18975be0a07a74df36
Javier Machin <javier_machin_acosta@hotmail.com>
2018-08-20 14:12:57 +0100

    Add event listener to toggle to-do details

--- src/projectRender.js
+++ src/projectRender.js
@@ -7,16 +7,16 @@ const Render = (projects) => {
     const projectText = document.createElement("p");
 
     projectText.innerHTML = `${project.getName()} | ${project.getDescription()}`;
-
-    projectContainer.appendChild(projectText);
-    
-    projectContainer.addEventListener("click", () => {
+ 
+    projectText.addEventListener("click", () => {
       let todoList = document.querySelector(".to-do-list");
 
       // Toggle to-do list
       (todoList) ? projectContainer.removeChild(todoList) : 
                    RenderTodo(projectContainer, project);
     });
+
+    projectContainer.appendChild(projectText);
       
     contentContainer.appendChild(projectContainer);
   });
@@ -26,21 +26,37 @@ const Render = (projects) => {
     const todoList = document.createElement("ol");
     todoList.classList.add("to-do-list");
     
-    todos.forEach((todo) => {
+    todos.forEach((todo, i) => {
       const todoListItem = document.createElement("li");
       const itemName = document.createElement("p");
-      const itemDescription = document.createElement("p");
-      const itemInfo = document.createElement("p");
-
-      itemName.innerHTML = todo.getName();
-      itemDescription.innerHTML = todo.getDescription();
-      itemInfo.innerHTML = `DueTime: ${todo.getDueTime()} |
-                            Priority: ${todo.getPriority()}`;
       
-      todoListItem.appendChild(itemName);
-      todoListItem.appendChild(itemDescription);
-      todoListItem.appendChild(itemInfo);
+      itemName.innerHTML = todo.getName();
+      itemName
+        
+      itemName.addEventListener("click", () => {
+        let todoDetails = document.getElementById(`.to-do-details-${i}`);
 
+        if (todoDetails) {
+          todoDetails.parentNode.removeChild(todoDetails);
+        } else {
+          todoDetails = document.createElement("section");
+          todoDetails.classList.add("to-do-details");
+          todoDetails.id = `.to-do-details-${i}`;
+          
+          const itemDescription = document.createElement("p");
+          const itemInfo = document.createElement("p");
+          
+          itemDescription.innerHTML = todo.getDescription();
+          itemInfo.innerHTML = `DueTime: ${todo.getDueTime()} |
+                                Priority: ${todo.getPriority()}`;
+          
+          todoDetails.appendChild(itemDescription);
+          todoDetails.appendChild(itemInfo);
+          todoListItem.appendChild(todoDetails);
+        }
+      });
+
+      todoListItem.appendChild(itemName);
       todoList.appendChild(todoListItem);
     });
 
